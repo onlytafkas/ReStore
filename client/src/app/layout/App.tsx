fe56572daeae02a58bb6ed8515a7d599e1718b37
tafkas <sven.roels@icloud.com>
2024-01-28 11:57:35 +0100

    intermed checkin 32

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import { Product } from "../models/product";
+import { Typography } from "@mui/material";
+import Catalog from "../../features/catalog/Catalog";
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+    {
+      id: prevState.length + 101,
+      name: 'product' + (prevState.length + 1),
+      price: ((prevState.length * 100) + 100),
+      brand: 'some brand',
+      description: 'some descr',
+      pictureUrl: 'http://picsum/.photos/200'
+    }])
+  }
+
+  return (
+    <>
+      <div className="app">
+        <Typography variant='h1'>Re-Store</Typography>
+        <Catalog products={products} addProduct={addProduct} />
+      </div>
+    </>
+  )
+}
+
+export default App
